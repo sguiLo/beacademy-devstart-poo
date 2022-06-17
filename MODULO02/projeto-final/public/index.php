69dfccd01cba0a6d2722329d301f4a7414ccb9a3
sguiLo <gustavoolr13@gmail.com>
2022-06-17 18:58:15 -0300

    Aula urls

--- MODULO02/projeto-final/public/index.php
+++ MODULO02/projeto-final/public/index.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'Olá mundo';
+include '../vendor/autoload.php';
+
+use App\Controller\IndexController;
+use App\Controller\ProductController;
+use App\Controller\ErrorController;
+
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+
+if ($url === '/'){
+    $c = new IndexController();
+    $c->indexAction();    
+} elseif ($url === '/login'){
+    $c = new IndexController();
+    $c->loginAction();
+} elseif ($url === '/produtos'){
+    $p = new ProductController();
+    $p->listAction();
+} else {
+    $e = new ErrorController();
+    $e->notFoundAction();
+}
+
+// echo 'Olá mundo';
